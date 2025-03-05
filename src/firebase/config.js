ba92b2bbbbded87c35fae882b976eb32a36304aa
m-ruzhynskyi <m.ruzhynskyi@gmail.com>
2025-03-05 12:19:57 +0200

    j012 config

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,15 +1,14 @@
-import {initializeApp} from "firebase/app";
-import {getAuth} from "firebase/auth";
-import {getFirestore} from "firebase/firestore";
-
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAGJCh2l6Dt9rcqpzRe3027pF1kD4r7jUE",
-  authDomain: "work-2ca8d.firebaseapp.com",
-  projectId: "work-2ca8d",
-  storageBucket: "work-2ca8d.appspot.com",
-  messagingSenderId: "1031605588142",
-  appId: "1:1031605588142:web:41c1d257cbe188e7229d3f"
+  apiKey: "AIzaSyCFL2fXXTqfufeI9KF_mwtv7wa5Can21Ww",
+  authDomain: "j012-698ab.firebaseapp.com",
+  projectId: "j012-698ab",
+  storageBucket: "j012-698ab.firebasestorage.app",
+  messagingSenderId: "665983905496",
+  appId: "1:665983905496:web:efa5548880c757686471ad",
 };
 
 const app = initializeApp(firebaseConfig);
@@ -17,4 +16,4 @@ const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
 
-export {auth, db}
+export { auth, db };
